4fd99e901398446738b71d1405c22582bae6b834
Yatharth <yatharthpawr@gmail.com>
2025-09-14 21:21:07 +0530

    Minor ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@ import { useRouter } from "next/navigation";
 import { useRef, useEffect } from "react";
 import { WebSocketConnHandle } from "@/utils/functions/waitForConnection";
 import { useApplicationContext } from "@/lib/context/ApplicationContext";
-import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 export default function DashBoard() {
   const router = useRouter();
   const roomIdRef = useRef<HTMLInputElement>(null);
@@ -35,56 +42,41 @@ export default function DashBoard() {
     };
   });
   return (
-    // <div className="flex w-full h-full bg-white items-center justify-center">
-    //   <div>
-    //     <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-    //       Toggle Theme
-    //     </Button>
-    //   </div>
-    //   <div>
-    //     <Button
-    //       onClick={() => {
-    //         //create  a new room,
-    //         if (!ws.current) {
-    //           return;
-    //         }
-    //         const wsConnMan = new WebSocketConnHandle(ws.current, 1800);
-    //         wsConnMan.waitForConnection(() => {
-    //           if (!ws.current) {
-    //             return;
-    //           }
-    //           ws.current.send(
-    //             JSON.stringify({
-    //               event: "createNewRoom",
-    //             })
-    //           );
-    //         });
-    //       }}
-    //     >
-    //       Create new room
-    //     </Button>
-    //     <input
-    //       ref={roomIdRef}
-    //       type="text"
-    //       className="bg-black h-10 w-32 mx-10 text-white px-2"
-    //       placeholder="Enter room ID"
-    //     />
-    //     <Button
-    //       onClick={() => {
-    //         const roomId = roomIdRef.current?.value?.trim();
-    //         localStorage.setItem("roomId", roomId as string);
-    //         console.log("The roomId being sent is,", roomId);
-    //         if (roomId) {
-    //           router.push(`/podcast/${roomId}`);
-    //         }
-    //       }}
-    //     >
-    //       Join a Room
-    //     </Button>
-    //   </div>
-    // </div>
-
     <div className="w-screen flex items-center justify-center flex-col">
+      <header className="mb-10 sticky top-0 z-50 w-full backdrop-blur-lg bg-transparent border-b border-border">
+        <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
+          <div>QwalPod</div>
+          <nav className="hidden items-center gap-4 md:flex lg:gap-8">
+            <a className="text-muted-foreground hover:text-foreground group relative text-xs font-medium transition-colors lg:text-sm hover:cursor-pointer">
+              hello
+              <span className="bg-primary absolute -bottom-1 left-0 h-0.5 w-0 transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+            <a className="text-muted-foreground hover:text-foreground group relative text-xs font-medium transition-colors lg:text-sm hover:cursor-pointer">
+              hello
+              <span className="bg-primary absolute -bottom-1 left-0 h-0.5 w-0 transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+            <a className="text-muted-foreground hover:text-foreground group relative text-xs font-medium transition-colors lg:text-sm hover:cursor-pointer">
+              Pricing
+              <span className="bg-primary absolute -bottom-1 left-0 h-0.5 w-0 transition-all duration-300 group-hover:w-full"></span>
+            </a>
+          </nav>
+          <div className="hidden cursor-pointer items-center gap-4 md:flex">
+            <div className="flex items-center gap-4">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <Button>Sign up</Button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </div>
+        </div>
+      </header>
       <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
         <div className="w-[250px] h-[200px] rounded-full bg-gradient-to-tr from-[#ff7b00] via-[#ffb800] to-[#ff7b00] opacity-50 blur-[120px]" />
       </div>
@@ -102,29 +94,28 @@ export default function DashBoard() {
       </p>
 
       <div>
-        <div>
-          <Button
-            onClick={() => {
-              //create  a new room,
+        <Button
+          onClick={() => {
+            //create  a new room,
+            if (!ws.current) {
+              return;
+            }
+            const wsConnMan = new WebSocketConnHandle(ws.current, 1800);
+            wsConnMan.waitForConnection(() => {
               if (!ws.current) {
                 return;
               }
-              const wsConnMan = new WebSocketConnHandle(ws.current, 1800);
-              wsConnMan.waitForConnection(() => {
-                if (!ws.current) {
-                  return;
-                }
-                ws.current.send(
-                  JSON.stringify({
-                    event: "createNewRoom",
-                  })
-                );
-              });
-            }}
-          >
-            Create new room
-          </Button>
-        </div>
+              ws.current.send(
+                JSON.stringify({
+                  event: "createNewRoom",
+                })
+              );
+            });
+          }}
+        >
+          Create new room
+        </Button>
+
         <input
           ref={roomIdRef}
           type="text"
